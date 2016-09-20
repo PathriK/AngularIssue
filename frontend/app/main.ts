d230f9f9352cdea6ed806d257cf85cc93f1cb9e8
drew.jocham <drew.jocham@gmail.com>
2016-09-20 09:13:17 -0400

    clean up project

--- frontend/app/main.ts
+++ frontend/app/main.ts
@@ -1,5 +1,3 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
+/**
+ * Created by KEYSTONE_USER on 9/20/16.
+ */
